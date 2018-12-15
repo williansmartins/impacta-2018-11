a2677516d341b6699abe863af2cacfb48bbf3478
Willians Martins <professorwillians@gmail.com>
2018-12-15 17:14:37 -0200

    add manipulando json

--- projeto-servlet-maven/src/main/java/br/com/impacta/Servlet1.java
+++ projeto-servlet-maven/src/main/java/br/com/impacta/Servlet1.java
@@ -5,11 +5,14 @@ package br.com.impacta;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
 
 @SuppressWarnings("serial")
 public class Servlet1 extends HttpServlet {
@@ -21,10 +24,50 @@ public class Servlet1 extends HttpServlet {
 	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		System.out.println("Servlet config called");
+		response.setContentType("application/json");
+		
+		String nome = request.getParameter("nome");
+		System.out.println("Nome: " + nome);
+		
 		PrintWriter out = response.getWriter();
-		out.println("<h1>" + message + "</h1>");
+		out.println("{   \"nome\" : \"  "+ nome + " \"   }");
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		
+		System.out.println("POST");
+		
+		Gson gson = new Gson();
+		 
+        try {
+            StringBuilder sb = new StringBuilder();
+            String s;
+            while ((s = request.getReader().readLine()) != null) {
+                sb.append(s);
+            }
+ 
+            Usuario usuario = (Usuario) gson.fromJson(sb.toString(), Usuario.class);
+ 
+            Status status = new Status();
+            if (usuario.getNome().equalsIgnoreCase("willians")) {
+                status.setSuccess(true);
+                status.setDescription("successo");
+            } else {
+                status.setSuccess(false);
+                status.setDescription("não é willians");
+            }
+            response.getOutputStream().print(gson.toJson(status));
+            response.getOutputStream().flush();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Status status = new Status();
+            status.setSuccess(false);
+            status.setDescription(ex.getMessage());
+            response.getOutputStream().print(gson.toJson(status));
+            response.getOutputStream().flush();
+        }
+		
 	}
 
 	public void destroy() {
---
+++ projeto-servlet-maven/src/main/java/br/com/impacta/Status.java
@@ -0,0 +1,20 @@
+package br.com.impacta;
+
+public class Status {
+    private boolean success;
+    private String description;
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+     
+    
+}

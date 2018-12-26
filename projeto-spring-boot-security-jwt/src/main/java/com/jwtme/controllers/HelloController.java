a8804d9127ea822a2d5fad8fa71a21e802c698c7
Willians Martins <willians.morais-e@riachuelo.com.br>
2018-12-26 17:19:44 -0200

    add projeto spring boot jwt ok

---
+++ projeto-spring-boot-security-jwt/src/main/java/com/jwtme/controllers/HelloController.java
@@ -0,0 +1,53 @@
+package com.jwtme.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/home")
+	public String hello() {
+		return "Hello buddy!";
+	}
+
+	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is welcome page!");
+		model.setViewName("hello");
+		return model;
+
+	}
+
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is protected page - Admin Page!");
+		model.setViewName("admin");
+
+		return model;
+
+	}
+	
+	@RequestMapping(value = "/dba**", method = RequestMethod.GET)
+	public ModelAndView dbaPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is protected page - Database Page!");
+		model.setViewName("admin");
+
+		return model;
+
+	}
+	
+	
+
+}
---
+++ projeto-spring-boot-security-jwt/src/main/java/com/jwtme/security/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.jwtme.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.jwtme.filter.JWTAuthenticationFilter;
+import com.jwtme.filter.JWTLoginFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable().authorizeRequests()
+//			.antMatchers("/welcome/**").access("hasRole('ROLE_ADMIN')")
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.antMatchers("/dba/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_DBA')")
+			.antMatchers("/home").permitAll()
+			.antMatchers(HttpMethod.POST, "/login").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			
+			// filtra requisições de login
+			.addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+	                UsernamePasswordAuthenticationFilter.class)
+			
+			// filtra outras requisições para verificar a presença do JWT no header
+			.addFilterBefore(new JWTAuthenticationFilter(),
+	                UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("willians").password("123456").roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("dba").password("123456").roles("DBA");
+	}
+}

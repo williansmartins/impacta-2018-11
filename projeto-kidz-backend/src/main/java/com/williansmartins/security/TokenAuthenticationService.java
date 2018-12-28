f717e8a2378e96afa06b3396428803b01198b664
Willians Martins <willians.morais-e@riachuelo.com.br>
2018-12-28 15:43:54 -0200

    inicio do backend do kiz, crud de usuario ok

---
+++ projeto-kidz-backend/src/main/java/com/williansmartins/security/TokenAuthenticationService.java
@@ -0,0 +1,105 @@
+package com.williansmartins.security;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import com.williansmartins.model.User;
+import com.williansmartins.service.UserService;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class TokenAuthenticationService {
+	
+	// EXPIRATION_TIME = 10 dias (860_000_000)
+	static final long EXPIRATION_TIME = 300_000;
+	static final String SECRET = "MySecret";
+	static final String TOKEN_PREFIX = "Bearer";
+	static final String HEADER_STRING = "Authorization";
+	
+	static void addAuthentication(HttpServletResponse response, Authentication auth, UserService userService) {
+		Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+		String tipo = "";
+		for (GrantedAuthority grantedAuthority : authorities) {
+			if("ADMIN".equals(grantedAuthority.getAuthority())) {
+				tipo = "admin";
+				break;
+			}
+		}
+		
+		String name = auth.getName();
+		User user = userService.findUserByEmail(name);
+		
+		String JWT = Jwts.builder()
+				.setSubject(name)
+				.claim("tipo", tipo)
+				.claim("id", user.getId())
+				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+				.signWith(SignatureAlgorithm.HS512, SECRET)
+				.compact();
+		
+		response.addHeader(HEADER_STRING, TOKEN_PREFIX + " " + JWT);
+	}
+	
+	static Authentication getAuthentication(HttpServletRequest request) {
+		String token = request.getHeader(HEADER_STRING);
+		
+		if (token != null && !"".equals(token)) {
+			// faz parse do token
+			try {
+				String user = Jwts.parser()
+						.setSigningKey(SECRET)
+						.parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+						.getBody()
+						.getSubject();
+				
+				if (user != null) {
+					return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+				}
+			}catch (JwtException e) {
+				return null;
+			}
+		}
+		return null;
+	}
+	
+	static Authentication getAdminAuthentication(HttpServletRequest request) {
+		String token = request.getHeader(HEADER_STRING);
+		
+		if (token != null && !"".equals(token)) {
+			try {
+				// faz parse do token
+				String user = Jwts.parser()
+						.setSigningKey(SECRET)
+						.parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+						.getBody()
+						.getSubject();
+				
+				
+				if (user != null) {
+					String tipo = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token.replace(TOKEN_PREFIX, "")).getBody().get("tipo").toString();
+					System.out.println(tipo);
+					
+					if("admin".equalsIgnoreCase(tipo)) {
+						return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+					}else {
+						return null;
+					}
+				}
+			}catch (JwtException e) {
+				return null;
+			}
+		}
+		return null;
+	}
+	
+}

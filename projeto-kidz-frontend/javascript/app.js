c629788ac8a304559447e6189d947d8fee06e79e
Willians Martins <professorwillians@gmail.com>
2019-01-05 09:17:40 -0200

    removendo projetos finalizados

--- projeto-kidz-frontend/javascript/app.js
+++
@@ -1,3 +0,0 @@
-var myApp = angular.module('principal',['ngRoute', 'ui.bootstrap', 'ngSanitize', 'base64', 'ngStorage', 'angular-logger', 'ui.utils.masks']);
-var barramento = "http://pwms.com.br/apps/barramento-homologacao/public/api/v1";
-var barramento = "http://localhost:8080";
--- projeto-kidz-frontend/javascript/filtros/global-filters.js
+++
@@ -1,8 +0,0 @@
-angular.module('principal')
-.filter('myDateFormat', function myDateFormat($filter){
-  return function(text){
-  	console.info(text);
-    var tempdate = new Date(text.replace(/-/g,"/")); 
-    return $filter('date')(tempdate, "dd/MM/yyyy");
-  }
-});

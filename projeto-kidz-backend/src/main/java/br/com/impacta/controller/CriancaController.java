9e5a7322d25c065965e19c897b68c9d28be44b5d
sa4915@CLASSROOM.NET <sa4915@CLASSROOM.NET>
2019-01-12 09:49:02 -0200

    Inclusão do @Controller no controler crianca

--- projeto-kidz-backend/src/main/java/br/com/impacta/controller/CriancaController.java
+++ projeto-kidz-backend/src/main/java/br/com/impacta/controller/CriancaController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.impacta.enumeric.SexoEnum;
 import br.com.impacta.model.Crianca;
-	
+
+@Controller
 @RequestMapping("/crianca")
 public class CriancaController {
 	

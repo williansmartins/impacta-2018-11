1c4ea1e94feb6801553d549ad3676f94d5912db6
Willians Martins <professorwillians@gmail.com>
2019-01-13 13:19:52 -0200

    atualizacao, add models, trocando senha, etc

--- projeto-spring-jpa-mysql/src/main/java/com/mkyong/Application.java
+++ projeto-spring-jpa-mysql/src/main/java/com/mkyong/Application.java
@@ -9,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.stream.Stream;
 
 import static java.lang.System.exit;
@@ -33,45 +36,69 @@ public class Application implements CommandLineRunner {
         SpringApplication.run(Application.class, args);
     }
 
-    @Transactional(readOnly = true)
     @Override
     public void run(String... args) throws Exception {
 
         System.out.println("DATASOURCE = " + dataSource);
-
-        System.out.println("\n1.findAll()...");
-        for (Customer customer : customerRepository.findAll()) {
-            System.out.println(customer);
-        }
-
-        System.out.println("\n2.findByEmail(String email)...");
-        for (Customer customer : customerRepository.findByEmail("222@yahoo.com")) {
-            System.out.println(customer);
-        }
-
-        System.out.println("\n3.findByDate(Date date)...");
-        for (Customer customer : customerRepository.findByDate(sdf.parse("2017-02-12"))) {
-            System.out.println(customer);
-        }
-
-        // For Stream, need @Transactional
-        System.out.println("\n4.findByEmailReturnStream(@Param(\"email\") String email)...");
-        try (Stream<Customer> stream = customerRepository.findByEmailReturnStream("333@yahoo.com")) {
-            stream.forEach(x -> System.out.println(x));
-        }
-
-        //System.out.println("....................");
-        //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        //Date from = sdf.parse("2017-02-15");
-        //Date to = sdf.parse("2017-02-17");
-
-        //for (Customer customer : customerRepository.findByDateBetween(from, to)) {
-        //    System.out.println(customer);
-        //}
-
+        
+//        inserir();
+//        buscar();
+//        findByEmail();
+//        findByDate();
+//        findByEmailReturnStream();
+//        findByDateBetween();
         System.out.println("Done!");
 
         exit(0);
     }
+
+	private void findByDateBetween() throws ParseException {
+//		System.out.println("....................");
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//        Date from = sdf.parse("2017-02-15");
+//        Date to = sdf.parse("2017-02-17");
+//
+//        for (Customer customer : customerRepository.findByDateBetween(from, to)) {
+//            System.out.println(customer);
+//        }
+	}
+
+	private void findByEmailReturnStream() {
+		// For Stream, need @Transactional
+        System.out.println("\n4.findByEmailReturnStream(@Param(\"email\") String email)...");
+        try (Stream<Customer> stream = customerRepository.findByEmailReturnStream("333@yahoo.com")) {
+            stream.forEach(x -> System.out.println(x));
+        }
+	}
+
+	private void findByDate() throws ParseException {
+		System.out.println("\n3.findByDate(Date date)...");
+        for (Customer customer : customerRepository.findByDate(sdf.parse("2017-02-12"))) {
+            System.out.println(customer);
+        }
+	}
+
+	private void findByEmail() {
+		System.out.println("\n2.findByEmail(String email)...");
+        for (Customer customer : customerRepository.findByEmail("222@yahoo.com")) {
+            System.out.println(customer);
+        }
+	}
+
+	private void buscar() {
+		System.out.println("\n1.findAll()...");
+        for (Customer customer : customerRepository.findAll()) {
+            System.out.println(customer);
+        }
+	}
+
+	private void inserir() {
+		Customer customer = new Customer();
+        customer.setDate(new Date());
+        customer.setEmail("email@teste.com");
+        customer.setName("willians");
+        
+		customerRepository.save(customer);
+	}
 
 }
---
+++ projeto-spring-jpa-mysql/src/main/java/com/mkyong/model/Cadastro.java
@@ -0,0 +1,38 @@
+package com.mkyong.model;
+
+public class Cadastro {
+
+	private Long id;
+	private String email;
+	private String senha;
+
+	@Override
+	public String toString() {
+		return email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}

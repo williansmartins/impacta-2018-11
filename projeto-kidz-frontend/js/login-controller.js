ef8eff9cc31b783d2d115c9ab49522b8a5e6db60
Willians Martins <professorwillians@gmail.com>
2019-01-19 17:37:07 -0200

    apontando login

--- projeto-kidz-frontend/js/login-controller.js
+++ projeto-kidz-frontend/js/login-controller.js
@@ -1,18 +1,28 @@
 var app = angular.module('myApp', []);
 
 app.controller('criancaCtrl', function($scope, $http) {
-
+ 
 	// $scope.email;
 
    	$scope.entrar = function(){
-   		$http.get("http://172.16.18.38:8081/cadastro")
-	   	.then(function (response) {
-	   		console.info(response);
-	   	});
+   		Jquery.post( "http://172.16.18.38:8080/auth", {
+            'username': $scope.email,
+            'password': $scope.senha
+        } );
+
+
+   		// $http({
+     //        method : "POST",
+     //        url : "http://172.16.18.38:8080/auth",
+     //        data: {
+     //            'username': $scope.email,
+     //            'password': $scope.senha
+     //        }
+     //    })
    	}
 
    	$scope.cadastrar = function(){
-	   	$http.get("http://172.16.18.38:8081/cadastro/" + $scope.senha)
+	   	$http.get("http://172.16.18.38:8080/cadastro/" + $scope.senha)
 	   	.then(function (response) {
 	   		console.info(response.data.senha);
 	   	});

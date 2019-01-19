1ef055550de4f2b74e1fdf02dc7d2bc56bf757a8
Willians Martins <professorwillians@gmail.com>
2019-01-19 13:33:29 -0200

    add spring security

---
+++ projeto-spring-jpa-mysql/src/main/java/br/com/impacta/SecurityConfig.java
@@ -0,0 +1,33 @@
+package br.com.impacta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+                   .withUser("willians").password("123456").roles("USER");
+	}
+
+	//.csrf() is optional, enabled by default, if using WebSecurityConfigurerAdapter constructor
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+	    http.authorizeRequests()
+		.antMatchers("/crianca/**").access("hasRole('ROLE_USER')")
+		.and()
+		    .formLogin().loginPage("/login").failureUrl("/login?error")
+		    .usernameParameter("username").passwordParameter("password")		
+		.and()
+		    .logout().logoutSuccessUrl("/login?logout")
+		.and()
+		    .csrf(); 		
+	}
+}
